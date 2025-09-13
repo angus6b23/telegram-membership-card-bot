52e1e8199449e758581f7d9e80be08834a40823c
Angus Wan <work@12a.app>
2025-09-13 19:11:43 +0100

    Try Harder when detecting code

--- utils/code-detect.ts
+++ utils/code-detect.ts
@@ -1,6 +1,12 @@
 import { readBarcodes } from "zxing-wasm/full";
 
 export const detectCode = async (image: Blob) => {
-  const res = await readBarcodes(image, { maxNumberOfSymbols: 1 });
+  const res = await readBarcodes(image, {
+    maxNumberOfSymbols: 1,
+    tryHarder: true,
+    tryDownscale: true,
+    tryRotate: true,
+    tryDenoise: true,
+  });
   return res[0];
 };
